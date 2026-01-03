6a8a58f6f9f160939c45c99b6a35e43a0617a298
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-01-03 12:50:55 +0100

    add (ex4) Done

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -1,7 +1,23 @@
 #!/bin/bash/python3
 
 
-def check_plant_health(plant_name, water_level: int, sunlight_hours) -> str:
+def check_plant_health(plant_name: str, water_level: int,
+                       sunlight_hours: int) -> str:
+    """Checks the plant health based on its values
+
+    Args:
+        plant_name (str): Name of the plant
+        water_level (int): Plant water level
+        sunlight_hours (int): Plant sunlight hours
+
+    Raises:
+        ValueError: Empty plant name
+        ValueError: Water level too low/high
+        ValueError: Sunlight hours too low/high
+
+    Returns:
+        str: Status of the plant
+    """
     if (plant_name == ""):
         raise ValueError("Plant name must be a non-empty string!")
     if (water_level < 1):
@@ -16,6 +32,9 @@ def check_plant_health(plant_name, water_level: int, sunlight_hours) -> str:
 
 
 def test_plant_checks() -> None:
+    """Test the check_plant_health function with different parameters
+    """
+    print("\n=== Good plant ===")
     try:
         plant_name = "Flower"
         water_level = 8
@@ -23,6 +42,30 @@ def test_plant_checks() -> None:
         print(check_plant_health(plant_name, water_level, sunlight_hours))
     except ValueError as e:
         print("Error :", e)
+    print("\n=== Bad plant name ===")
+    try:
+        plant_name = ""
+        print(check_plant_health(plant_name, water_level, sunlight_hours))
+    except ValueError as e:
+        print("Error :", e)
+
+    print("\n=== Bad water level ===")
+    try:
+        plant_name = "Flower"
+        water_level = 0
+        print(check_plant_health(plant_name, water_level, sunlight_hours))
+    except ValueError as e:
+        print("Error :", e)
+    print("\n=== Bad sunlight hours ===")
+    try:
+        water_level = 8
+        sunlight_hours = 0
+        print(check_plant_health(plant_name, water_level, sunlight_hours))
+    except ValueError as e:
+        print("Error :", e)
+    print("\nAll error raising tests are \
+          completed and the program did not crash!")
+    return (None)
 
 
 test_plant_checks()
